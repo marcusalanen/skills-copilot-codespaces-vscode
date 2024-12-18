48462133c79ffcd48d8e8bf6bd3674191d3a110b
Marcus Alanen <marcus.alanen@visma.com>
2024-12-18 14:06:37 +0200

    fixed create => Create

--- member.js
+++ member.js
@@ -1,3 +1,11 @@
+/**
+ * Creates a member object with predefined properties.
+ *
+ * @returns {Object} An object representing a member with name, age, and skills properties.
+ * @returns {string} return.name - The name of the member.
+ * @returns {number} return.age - The age of the member.
+ * @returns {string[]} return.skills - An array of skills the member possesses.
+ */
 function skillsMember(){
   var member = {
     name: 'John Doe',
